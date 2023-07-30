c14922b9c6a2d98e8c91f3870c5a74f960975fc1
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2023-07-30 23:09:53 +0300

    1

--- GI.WPF.SplitCatalogImg/MainWindow.xaml.cs
+++ GI.WPF.SplitCatalogImg/MainWindow.xaml.cs
@@ -30,61 +30,107 @@ namespace GI.WPF.SplitCatalogImg
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            string nameFileConfig = "NameCatalog1.txt";
-            string nameCatalog = "";
-            if (!isExistsCatalog(nameFileConfig))
-            {            
-                var dialog = new WindowGetNameCatalog();
-                if (dialog.ShowDialog() == true)
-                {
-                    nameCatalog = dialog.ResponseText;       
-                }
+            string nameFileConfig = "fileConfig.txt";
+            string nameCatalog = GetNameCatalog(nameFileConfig);
+
+            if (Directory.Exists(nameCatalog))
+            {
+                Title = "SplitCatalog:  " + nameCatalog;
             }
-            //Запись нового пути к каталогу в файл
+
+
+            //if (isExistsCatalog(nameCatalog))
+            //{
+            //    Title = "SplitCatalog:  " + nameCatalog;
+            //}
+           
+
+
+            //string nameFileConfig = "NameCatalog1.txt";
+            //string nameCatalog = "";
+            //if (!isExistsCatalog(nameFileConfig))
+            //{            
+            //    var dialog = new WindowGetNameCatalog();
+            //    if (dialog.ShowDialog() == true)
+            //    {
+            //        nameCatalog = dialog.ResponseText;       
+            //    }
+            //}
+            ////Запись нового пути к каталогу в файл
+            //using (FileStream fileStream = File.Open(nameFileConfig, FileMode.Create))
+            //{
+            //    using (StreamWriter output = new StreamWriter(fileStream))
+            //    {                   
+            //        output.Write(nameCatalog);
+            //    }
+            //}
+
+
+
+            //nameCatalog = GetNameCatalog(nameFileConfig);
+            //if (!isExistsCatalog(nameCatalog))
+            //{
+            //    nameCatalog = "Каталог " + nameCatalog + " не найден!";
+            //}
+            //Title = "SplitCatalog:  "  + nameCatalog ;
+            //// "C:\test\0"
+        }
+
+        //private bool isExistsCatalog(string nameCatalog) 
+        //{    
+        //    if (!Directory.Exists(nameCatalog))
+        //    {        
+        //        return false;
+        //    }
+        //    else
+        //        return true;
+        //}
+
+
+
+        private string GetNameCatalog(string nameConfigFile)
+        {
+            if (System.IO.File.Exists(nameConfigFile))
+            {
+                string text = new StreamReader(nameConfigFile).ReadLine();
+                return text;
+            }
+
+            return "";
+        }
+
+        private void MenuItem_OpenNewCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            string nameCatalog = "";
+            var dialog = new WindowGetNameCatalog();
+            if (dialog.ShowDialog() == true)
+            {
+                nameCatalog = dialog.ResponseText;
+            }
+            if (!Directory.Exists(nameCatalog))
+            {
+                MessageBox.Show("Каталог " + nameCatalog + " не обнаружен!");
+                Title = "SplitCatalog:";
+                return;
+            }
+
+            //if (!isExistsCatalog(nameCatalog))
+            //{
+            //    MessageBox.Show("Каталог " + nameCatalog + " не обнаружен!");
+            //    return;
+            //}          
+
+
+            string nameFileConfig = "fileConfig.txt";
             using (FileStream fileStream = File.Open(nameFileConfig, FileMode.Create))
             {
                 using (StreamWriter output = new StreamWriter(fileStream))
-                {                   
+                {
                     output.Write(nameCatalog);
                 }
             }
 
-
-
-            nameCatalog = GetNameCatalog(nameFileConfig);
-            if (!isExistsCatalog(nameCatalog))
-            {
-                nameCatalog = "Каталог " + nameCatalog + " не найден!";
-            }
-            Title = "SplitCatalog:  "  + nameCatalog ;
-            // "C:\test\0"
+            Title = "SplitCatalog:  " + nameCatalog;
         }
-
-        private bool isExistsCatalog(string nameCatalog) 
-        {    
-            if (!Directory.Exists(nameCatalog))
-            {        
-                return false;
-            }
-            else
-                return true;
-        }
-
-
-
-        private string GetNameCatalog(string nameCatalog)
-        {
-            if (!isExistsCatalog(nameCatalog))
-            {
-                MessageBox.Show("Каталог " + nameCatalog + " не обнаружен!");
-                return "";
-            }
-                    
-
-            string text = new StreamReader(nameCatalog).ReadLine();
-            return text;
-        }
-
-
     }
 }
