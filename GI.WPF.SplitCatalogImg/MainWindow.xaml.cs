a52c7e5436e163b915fbefd174c669fd08563d85
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2023-08-07 01:20:32 +0300

    Заполнение

--- GI.WPF.SplitCatalogImg/MainWindow.xaml.cs
+++ GI.WPF.SplitCatalogImg/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -65,15 +66,60 @@ namespace GI.WPF.SplitCatalogImg
             TextBox_BeforeCounter.Text = "";
             TextBox_AfterCounter.Text = "";
 
-            var a = Directory.GetFiles(currentCatalog);
+            var agr = SortFilesList();
 
 
             TextBox_BeginningCounter.Text = "1";          
-            TextBox_CountFiles.Text = "";
-            TextBox_CountСharacter.Text = "";
-          
+            TextBox_CountFiles.Text = "" + agr.Count;
+
+            if(agr.Count<10)
+                TextBox_CountСharacter.Text = "1";
+            else if (agr.Count < 100)
+                TextBox_CountСharacter.Text = "2";
+            else if (agr.Count < 1000)
+                TextBox_CountСharacter.Text = "3";
+
+            DataGrid_NameFiles.ItemsSource = agr;
 
         }
+
+
+        private List<string> SortFilesList()
+        {
+            List<string> sortFilesList = new List<string>(Directory.GetFiles(currentCatalog));
+
+            /*Сортируем*/
+            sortFilesList.Sort(delegate (string x, string y)
+            {
+                string temp1 = x.Substring(x.LastIndexOf('\\') + 1);
+                string temp2 = y.Substring(y.LastIndexOf('\\') + 1);
+
+                string[] substr = { "\\", ".jpg", ".png", ".gif", ".webn"};
+                foreach (string sub in substr)
+                {
+                    temp1 = temp1.Replace(sub, "");
+                    temp2 = temp2.Replace(sub, "");
+                }
+
+                int number1; int number2;
+                if (int.TryParse(temp1, out number1) && int.TryParse(temp2, out number2))
+                {
+                    return number1 - number2;
+                }
+                else if (int.TryParse(temp1, out number1) || int.TryParse(temp2, out number2))
+                {
+                    return x.CompareTo(y);
+                }
+                else
+                {
+                    return x.CompareTo(y);
+                }
+            });
+
+            return sortFilesList;
+        }
+
+
 
 
 
